a959d58cd76a6409adccde576915296e950af787
Mostafa Ehab <mostafaehab0002@gmail.com>
2024-09-09 02:02:30 +0300

    Linked Lists append & prepend methods

--- Data Structures/linkedList.js
+++ Data Structures/linkedList.js
@@ -1,14 +1,53 @@
+//? Create Linked List: 10 --> 25 --> 300
+
 class LinkedList {
-  //! Initiating linked list => Initiating head only
+  ////////////* Initiating linked list //////////
+  //! Initiating head only
   constructor(headValue) {
-    this.head = {
-      value: headValue,
-      next: null,
-    };
+    this.head = { value: headValue, next: null };
+
     //! Tail equals head as list is empty @ initiation
     this.tail = this.head;
+
+    //! Only head exists @ initiation
+    this.length = 1;
+  }
+
+  ////////////* Methods /////////////
+  append(nodeValue) {
+    const newNode = { value: nodeValue, next: null };
+
+    //! Change pointer of tail: null => New Node
+    //? Must update pointer of tail before changing tail to be new node
+    this.tail.next = newNode;
+
+    //! New node becomes the tail
+    this.tail = newNode;
+
+    //! Update length
+    this.length++;
+
+    return this; // return linked list
+  }
+
+  prepend(nodeValue) {
+    const newNode = { value: nodeValue, next: this.head };
+
+    this.head = newNode;
+
+    this.length++;
+
+    return this;
   }
 }
 
-//* Create Linked List: 10 --> 25 --> 300
 const mostafaLinkedList = new LinkedList(10);
+console.log("Initialization:\n", mostafaLinkedList);
+
+mostafaLinkedList.append(25);
+mostafaLinkedList.append(300);
+console.log("\nAppending Nodes:\n", mostafaLinkedList);
+
+mostafaLinkedList.prepend("Hello");
+mostafaLinkedList.prepend("World");
+console.log("\nPrepending Nodes:\n", mostafaLinkedList);
