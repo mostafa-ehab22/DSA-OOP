9f5d96d8a981e47e9c72fe2d9b41edc4b3fff447
Mostafa Ehab <mostafaehab0002@gmail.com>
2024-09-12 01:43:02 +0300

    Stacks

---
+++ Data Structures/stackArray.js
@@ -0,0 +1,31 @@
+//*  Push | Pop | Length  ==> Already built-in arrays
+
+class Stack {
+  constructor() {
+    this.array = [];
+  }
+
+  peek() {
+    //! Access last index of array
+    return this.array[this.array.length - 1];
+  }
+
+  push(value) {
+    this.array.push(value);
+    return this;
+  }
+
+  pop() {
+    this.array.pop();
+    return this;
+  }
+}
+
+const myStack = new Stack();
+
+console.log(myStack.push("FIRST"));
+console.log(myStack.push("SECOND"));
+
+console.log(myStack.peek());
+
+console.log(myStack.pop());
--- Data Structures/stackLinkedList.js
+++ Data Structures/stackLinkedList.js
@@ -19,19 +19,41 @@ class Stack {
   peek() {
     return this.top;
   }
+
   push(value) {
     const newNode = new Node(value);
 
     if (this.length === 0) {
       this.top = this.bottom = newNode;
     } else {
-      this.top.next = newNode;
+      //! Hold Old top item
+      holdingPointer = this.top;
+
+      //! Add new item & point to old item
       this.top = newNode;
+      this.top.next = holdingPointer;
     }
 
     this.length++;
     return this;
   }
 
-  pop() {}
+  pop() {
+    if (!this.top) {
+      return null;
+    }
+
+    //! ONLY 1 Item left in stack
+    if (this.top === this.bottom) {
+      this.bottom = null;
+    }
+
+    //* const holdingPointer = this.top           ==> USED to return popped item
+    this.top = this.top.next;
+
+    this.length--;
+    return this;
+
+    //* return holdingPointer
+  }
 }
