7b7befb7f2359e86bec4526db8115e94bf0c7622
Mostafa Ehab <mostafaehab0002@gmail.com>
2024-09-12 15:51:56 +0300

    Queues implementation

--- Data Structures/queue.js
+++ Data Structures/queue.js
@@ -5,6 +5,12 @@
   *  (((First: REMOVING)))     [5] -> [4] -> [3] -> [2] -> [1] -> newNode     (((Last: ADDING)))
 */
 
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
 class Queue {
   constructor() {
     this.first = null;
@@ -12,9 +18,46 @@ class Queue {
     this.length = 0;
   }
 
-  peek() {}
-  enqueue() {}
-  dequeue() {}
+  peek() {
+    return this.first ? this.first.value : null;
+  }
+  enqueue(value) {
+    const newNode = new Node(value);
+
+    if (this.length === 0) {
+      this.first = this.last = newNode;
+    } else {
+      this.last.next = newNode;
+      this.last = newNode;
+    }
+
+    this.length++;
+    return this;
+  }
+  dequeue() {
+    if (this.first === this.last) {
+      return null;
+    }
+
+    const holdingPointer = this.first;
+    this.first = this.first.next;
+
+    if (this.length === 1) {
+      this.last = null;
+    }
+
+    this.length--;
+    return holdingPointer.value;
+  }
 }
 
 const myQueue = new Queue();
+
+console.log(myQueue.enqueue("Mostafa"));
+console.log(myQueue.enqueue("Ehab"));
+console.log(myQueue.enqueue("Yehia"));
+
+console.log("DEQUEUED:", myQueue.dequeue());
+console.log("DEQUEUED:", myQueue.dequeue());
+
+console.log(myQueue.peek());
