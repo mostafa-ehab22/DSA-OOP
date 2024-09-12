b7a2d730ca12cb29a488ad03ff3a27ddfbc06127
Mostafa Ehab <mostafaehab0002@gmail.com>
2024-09-12 13:24:35 +0300

    Update Stacks

--- Data Structures/practice.js
+++ Data Structures/practice.js
@@ -1,110 +1,46 @@
 class Node {
   constructor(value) {
     this.value = value;
-    this.next = null;
+    this.next = next;
   }
 }
 
-class LinkedList {
-  constructor(value) {
-    this.head = { value: value, next: null };
-
-    this.tail = this.head;
-
-    this.length = 1;
+class Stack {
+  constructor() {
+    this.top = null;
+    this.bottom = null;
+    this.length = 0;
   }
 
-  append(value) {
-    let newNode = new Node(value);
-
-    this.tail.next = newNode;
-    this.tail = newNode;
-    this.length++;
-  }
-  prepend(value) {
-    let newNode = new Node(value);
-
-    newNode.next = this.head;
-    this.head = newNode;
-    this.length++;
+  peek() {
+    return this.top;
   }
 
-  printList() {
-    const array = [];
-    let currentNode = this.head;
-
-    while (currentNode !== null) {
-      array.push(currentNode.value);
-      currentNode = currentNode.next;
-    }
-    console.log(array);
-  }
-
-  traverseToIndex(index) {
-    let currentNode = this.head;
-    let counter = 0;
-
-    while (counter !== index) {
-      currentNode = currentNode.next;
-      counter++;
-    }
-
-    return currentNode;
-  }
-
-  insert(index, value) {
+  push(value) {
     const newNode = new Node(value);
 
-    const leader = this.traverseToIndex(index - 1);
-    const ending = leader.next;
-
-    leader.next = newNode;
-    newNode.next = ending;
-
+    if (this.length === 0) {
+      this.top = this.bottom = newNode;
+    } else {
+      newNode.next = this.top;
+      this.top = newNode;
+    }
     this.length++;
-
-    console.log("INSERTING\n", this.printList());
+    return this;
   }
 
-  remove(index) {
-    if (index === 0) {
-      this.head = this.head.next;
-      this.length--;
-      return;
+  pop() {
+    if (this.length === 0) return null;
+
+    if (this.top === this.bottom) {
+      this.bottom = null;
     }
 
-    const leader = this.traverseToIndex(index - 1);
-    const ending = this.traverseToIndex(index + 1);
+    const poppedItem = this.top;
 
-    leader.next = ending;
-
+    this.top = this.top.next;
     this.length--;
-  }
 
-  reverse() {
-    let first = this.head;
-    let second = first.next;
-    this.tail = this.head;
-
-    while (second) {
-      const third = second.next;
-
-      second.next = first;
-      first = second;
-      second = third;
-    }
-
-    this.head.next = null;
-    this.head = first;
-
-    this.printList();
+    return poppedItem;
   }
 }
-
-const linkedListPractice = new LinkedList(10);
-
-linkedListPractice.prepend("Start");
-linkedListPractice.append("End");
-
-linkedListPractice.printList();
-linkedListPractice.reverse();
--- Data Structures/stackLinkedList.js
+++ Data Structures/stackLinkedList.js
@@ -5,8 +5,9 @@ class Node {
   }
 }
 /*
-  Stack (from top to bottom):
-    Top --> [5] -> [4] -> [3] -> [2] -> [1] -> null
+  ! LIFO
+  Stack (from top to bottom) => LINKED LIST PREPENDING
+    *TOP*     [5] -> [4] -> [3] -> [2] -> [1] -> null     *BOTTOM*
 */
 
 class Stack {
@@ -23,13 +24,14 @@ class Stack {
   push(value) {
     const newNode = new Node(value);
 
+    //! 1st item added
     if (this.length === 0) {
       this.top = this.bottom = newNode;
     } else {
-      //! Hold Old top item
+      //* Hold Old top item
       holdingPointer = this.top;
 
-      //! Add new item & point to old item
+      //* Add new item & point to old item
       this.top = newNode;
       this.top.next = holdingPointer;
     }
@@ -39,21 +41,19 @@ class Stack {
   }
 
   pop() {
-    if (!this.top) {
-      return null;
-    }
+    //! Stack already empty
+    if (this.length === 0) return null;
 
     //! ONLY 1 Item left in stack
     if (this.top === this.bottom) {
-      this.bottom = null;
+      this.bottom = null; // Also resets top to null
     }
 
-    //* const holdingPointer = this.top           ==> USED to return popped item
-    this.top = this.top.next;
+    const poppedItem = this.top; // Store the top node to return it later
 
+    this.top = this.top.next; // Move top to the next node
     this.length--;
-    return this;
 
-    //* return holdingPointer
+    return poppedItem;
   }
 }
